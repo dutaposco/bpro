e45f35771425f181574f5f897d5641c704e60590
dutaposco <dutaposco@gmail.com>
2026-04-01 15:03:54 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,7 +20,7 @@
                     </div>
                 </div>
                 <div class="hidden md:block">
-                    <img src="https://bpro.nnmaa.com/BrownBrother.jpg?w=600&h=400&fit=crop" alt="Football" class="rounded-lg shadow-2xl">
+                    <img src="https://bpro.nnmaa.com/BrownBrother.jpg" alt="Football" class="rounded-lg shadow-2xl">
                 </div>
             </div>
         </div>
